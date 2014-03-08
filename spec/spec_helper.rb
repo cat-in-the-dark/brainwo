39e76a80d818fe11819daa0410844a4eeed8a1d2
blan4 <ilya.blan4@gmail.com>
2014-03-08 12:40:36 +0700

    add coveralls integration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,7 @@
+require 'coveralls'
+Coveralls.wear!
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
