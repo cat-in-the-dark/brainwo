5085068ce53164658ee0466301b1deaa82425883
blan4 <ilya.blan4@gmail.com>
2014-03-15 16:09:08 +0700

    fix reloaading function

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -10,6 +10,7 @@ class QuizzesController < ParticipantsController
   def show
     @game = GameService.new(Quiz.started.find(params[:id]))
     @question = @game.current_question
+    @question_id = @question.id if @question
 
     stick_to_quiz @game.quiz
   rescue
@@ -28,7 +29,7 @@ class QuizzesController < ParticipantsController
       render json: {reload: false}
     end
   rescue
-    logger.info "RELOAD true + fail"
-    render json: {reload: true}
+    logger.info "RELOAD false + fail"
+    render json: {reload: false}
   end
 end
