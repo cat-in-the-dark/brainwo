a85dca87f9590b547a2d324ab8a84d6c475c3026
blan4 <ilya.blan4@gmail.com>
2014-03-20 20:35:58 +0700

    add base controllers for judgement

--- app/controllers/owner/judgement_controller.rb
+++ app/controllers/owner/judgement_controller.rb
@@ -10,12 +10,12 @@ class Owner::JudgementController < OwnerController
   def hurt
     @team = @game.teams.find_by(id: params[:tema_id])
     #TODO call hurting service
-    response json: {status: :accepted, pain: 0 }
+    render json: {status: :accepted, pain: 0 }
   end
 
-  def kill
-    @team = @game.teams.find_by(id: params[:tema_id])
-    #TODO call hurnig service and kill team
-    response json: { status: :updated }
+  def toggle_team_status
+    @team = @game.teams.find_by(id: params[:team_id])
+    #TODO call hurnig service and kill team or make alive
+    render json: { status: :updated, life_status: @team.status }
   end
 end
--- app/controllers/owner/teams_controller.rb
+++ app/controllers/owner/teams_controller.rb
@@ -1,10 +1,19 @@
 class Owner::TeamsController < OwnerController
   def index
+    @teams = quiz.temas
   end
 
   def new
+    @team = quiz.teams.build
+  end
+
+  def create
   end
 
   def edit
+    @team = quiz.teams.find params[:id]
+  end
+
+  def update
   end
 end
