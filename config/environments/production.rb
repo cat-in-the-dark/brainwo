0188d5ad784d785b884e6aa65bffe0f2677ec631
blan4 <ilya.blan4@gmail.com>
2014-04-12 16:43:02 +0700

    add owner.js to assets compiler in producction

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,8 @@ Brainwo::Application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
+  config.assets.precompile += %w( owner.js )
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
