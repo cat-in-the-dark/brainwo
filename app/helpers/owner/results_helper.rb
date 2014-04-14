6f1b0b7af8486c0fbaff1abfc3f0be6c4dd22c13
blan4 <ilya.blan4@gmail.com>
2014-04-14 10:48:42 +0700

    fix questions style

--- app/helpers/owner/results_helper.rb
+++ app/helpers/owner/results_helper.rb
@@ -1,2 +1,11 @@
 module Owner::ResultsHelper
+  def correcy_class(team, question)
+    return '' if question.unused?
+    team.is_right_for?(question) ? "success" : "danger"
+  end
+
+  def correcy_text(team, question)
+    return 'O' if question.unused?
+    team.is_right_for?(question) ? "V" : "X"
+  end
 end
